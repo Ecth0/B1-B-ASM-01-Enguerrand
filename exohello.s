686cddea90e93564f3ba2f84c926bf7f562cba29
Enguerrand <enguerrand.le-tourneur-hugon@oteria.fr>
2026-02-01 23:31:43 +0100

    Changement taille pour tout afficher

--- exohello.s
+++ exohello.s
@@ -1,14 +1,14 @@
 global main
 main:
-    mov rax, 0x646c726f77206f
+    mov rax, 0x00646c726f77
     push rax
-    mov rax, 0x6c6c6568
+    mov rax, 0x6f6c6c6568
     push rax
 
     mov rax, 1
     mov rdi, 1
     mov rsi, rsp
-    mov rdx, 12
+    mov rdx, 14
     syscall
 
     mov rax, 60
